68442aebd5a2a705864a4f462d2e4032cd38c636
rlexus90 <rlexus90@gmail.com>
2023-01-29 11:43:37 +0200

    feat: done task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {
+		this.guessNumber = 0;
+		this.range = [];
+	}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		for (let i = min; i <= max; i++) {
+			this.range.push(i);
+		}
+	}
 
-    }
+	guess() {
+		this.guessNumber = Math.ceil(
+			(this.range[0] + this.range[this.range.length - 1]) / 2);
+		return this.guessNumber;
+	}
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	lower() {
+		let index = this.range.indexOf(this.guessNumber);
+		this.range = this.range.slice(0, index+1);
+	}
+	greater() {
+		let index = this.range.indexOf(this.guessNumber);
+		this.range = this.range.slice(index );
+	}
 }
 
 module.exports = GuessingGame;
